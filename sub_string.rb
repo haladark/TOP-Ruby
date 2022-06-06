406a27353795868932aef89d8be22ef8615fe289
Hala <43832921+haladark@users.noreply.github.com>
2022-06-06 15:16:11 -0400

    Add files via upload

--- sub_string.rb
+++ sub_string.rb
@@ -3,17 +3,19 @@
 # in the original string and how many times it was found.
 
 def substrings(string, dictionary)
-    matches = Hash.new
+    matched = Hash.new
  # no need to split it into array
  # RegExp of: (?=abc) => positive lookahead
+ 
     dictionary.each do |word|
-        matches[word] = string.downcase.scan(/(?=#{word})/).count if string.downcase.include?(word)
+        matched[word] = string.downcase.scan(/(?=#{word})/).count if string.downcase.include?(word)
     end
 
-    matches
+    puts matched
+   
 
 end
 
 my_string = "Howdy partner 123 , sit down! 456 How's it going?"
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-puts substrings(my_string,dictionary)
+substrings(my_string,dictionary)
